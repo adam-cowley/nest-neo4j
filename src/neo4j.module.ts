6418e70d4d272ca45a03c93f896b1c4abb5359e0
Adam Cowley <github@adamcowley.co.uk>
2020-10-19 14:25:12 +0100

    Added fromEnv method to extract config from .env file

--- src/neo4j.module.ts
+++ src/neo4j.module.ts
@@ -1,5 +1,5 @@
 import { Module, DynamicModule, Provider } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Neo4jService } from './neo4j.service';
 import { Neo4jConfig } from './interfaces/neo4j-config.interface';
 import { NEO4J_OPTIONS, NEO4J_DRIVER } from './neo4j.constants';
@@ -44,6 +44,39 @@ export class Neo4jModule {
                 {
                     provide: NEO4J_OPTIONS,
                     ...configProvider
+                } as Provider<any>,
+                {
+                    provide: NEO4J_DRIVER,
+                    inject: [ NEO4J_OPTIONS ],
+                    useFactory: async (config: Neo4jConfig) => createDriver(config),
+                },
+                Neo4jService,
+            ],
+            exports: [
+                Neo4jService,
+            ]
+        }
+    }
+
+    static fromEnv(): DynamicModule {
+        return {
+            module: Neo4jModule,
+            global: true,
+            imports: [
+                ConfigModule,
+            ],
+            providers: [
+                {
+                    provide: NEO4J_OPTIONS,
+                    inject: [ ConfigService ],
+                    useFactory: (configService: ConfigService) : Neo4jConfig => ({
+                        scheme: configService.get('NEO4J_SCHEME'),
+                        host: configService.get('NEO4J_HOST'),
+                        port: configService.get('NEO4J_PORT'),
+                        username: configService.get('NEO4J_USERNAME'),
+                        password: configService.get('NEO4J_PASSWORD'),
+                        database: configService.get('NEO4J_DATABASE'),
+                    })
                 } as Provider<any>,
                 {
                     provide: NEO4J_DRIVER,

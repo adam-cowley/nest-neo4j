cfe616effec1d1a103c00fff4bbe7c096a958e74
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-04-13 14:26:17 +0430

    refactor: Remove unused code
    - add prettier formatter to project

--- src/interfaces/neo4j-config.interface.ts
+++ src/interfaces/neo4j-config.interface.ts
@@ -1,10 +1,16 @@
-export type Neo4jScheme = 'neo4j' | 'neo4j+s' | 'neo4j+scc' | 'bolt' | 'bolt+s' | 'bolt+scc'
+export type Neo4jScheme =
+  | 'neo4j'
+  | 'neo4j+s'
+  | 'neo4j+scc'
+  | 'bolt'
+  | 'bolt+s'
+  | 'bolt+scc';
 
 export interface Neo4jConfig {
-    scheme: Neo4jScheme;
-    host: string;
-    port: number | string;
-    username: string;
-    password: string;
-    database?: string;
+  scheme: Neo4jScheme;
+  host: string;
+  port: number | string;
+  username: string;
+  password: string;
+  database?: string;
 }
